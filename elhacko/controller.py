18180e88111fdf097195bdc396a5442abe4a6f8b
Flyte <flyte@failcode.co.uk>
2015-11-15 22:15:16 +0000

    Use crochet to enable synchronous websocket publishing

--- elhacko/controller.py
+++ elhacko/controller.py
@@ -1,14 +1,34 @@
+import sys
 import logging
 import argparse
+import traceback
+from time import sleep
 
 import zmq
 import zerorpc
+import crochet
 from transitions import Machine
-from transitions import logger as sm_logger
+from transitions import logger
+
+crochet.setup()  # Must be called before any twisted/autobahn imports
+from autobahn.twisted.wamp import Application
 
 
-sm_logger.addHandler(logging.StreamHandler())
-sm_logger.setLevel(logging.INFO)
+logger.setLevel(logging.INFO)
+ws_app = Application()
+
+
+@crochet.run_in_reactor
+def start_ws_app(ws_uri, ws_realm):
+    ws_app.run(ws_uri, ws_realm, start_reactor=False)
+
+
+@crochet.wait_for(timeout=1)
+def ws_publish(topic, *args, **kwargs):
+    """
+    Publish to a topic on the websocket server.
+    """
+    return ws_app.session.publish(topic, *args, **kwargs)
 
 
 class ElhackoController:
@@ -31,54 +51,91 @@ class ElhackoController:
     ]
     sm = None
     uuid = None
+    img_data = None
 
-    def __init__(self, qr_uri, qr_prefix):
+    def __init__(self, qr_uri, qr_prefix, img_cap_uri, ws_uri, ws_realm):
         self.machine = Machine(
             model=self,
             states=ElhackoController.states,
             initial="awaiting_uuid",
             transitions=ElhackoController.transitions
         )
-        
+
         self.context = zmq.Context()
+
+        # QR reader set up
         s = self.context.socket(zmq.SUB)
         s.connect(qr_uri)
         s.setsockopt_string(zmq.SUBSCRIBE, unicode(qr_prefix))
         self.qr_socket = s
         self.qr_prefix = qr_prefix
 
+        # Image Capture set up
+        self.img_cap_uri = img_cap_uri
+        c = zerorpc.Client()
+        c.connect(img_cap_uri) 
+        self.img_cap_client = c
+
+        # Websocket set up
+        self.ws_uri = ws_uri
+        self.ws_realm = ws_realm
+
     def get_uuid(self):
         """
         Waits for the QR reading service to produce a UUID.
         """
         self.uuid = self.qr_socket.recv()[len(self.qr_prefix):]
-        sm_logger.info("Got UUID: %s" % self.uuid)
+        logger.info("Got UUID: %s" % self.uuid)
         self.got_uuid()
 
     def start_countdown(self):
+        """
+        Publishes the "countdown" message to the websocket server on the uuid channel.
+        """
+        ws_publish(self.uuid, "COUNTDOWN")
         self.started_countdown()
 
     def get_img(self):
+        """
+        Orders the image capture service to take a picture and retrieves the image.
+        """
+        for i in range(3):
+            logger.info(3-i)
+            sleep(1)
+        logger.info("Say cheese!")
+        self.img_data = self.img_cap_client.take_photo()
         self.got_img()
 
     def save_img(self):
+        sleep(1)
         self.saved_img()
 
     def send_img(self):
+        ws_publish(self.uuid, "Here is the picture..")
         self.sent_img()
 
     def reset(self):
         self.uuid = None
-        # self.reset()
+        self.img_data = None
 
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument("qr_uri")
     p.add_argument("qr_prefix")
+    p.add_argument("img_cap_uri")
+    p.add_argument("ws_uri", type=unicode)
+    p.add_argument("ws_realm", type=unicode)
     args = p.parse_args()
 
-    controller = ElhackoController(args.qr_uri, args.qr_prefix)
+    controller = ElhackoController(
+        args.qr_uri,
+        args.qr_prefix,
+        args.img_cap_uri,
+        args.ws_uri,
+        args.ws_realm
+    )
+    start_ws_app(args.ws_uri, args.ws_realm)
 
     while True:
         try:
@@ -87,7 +144,10 @@ if __name__ == "__main__":
             controller.get_img()
             controller.save_img()
             controller.send_img()
+        except KeyboardInterrupt:
+            sys.exit(0)
         except Exception as e:
-            sm_logger.error("Exception: %s" % e)
+            logger.error("Exception: %s" % e)
+            logger.error(traceback.format_exc())
         finally:
             controller.reset()
